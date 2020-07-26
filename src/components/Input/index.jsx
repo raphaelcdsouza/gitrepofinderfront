1dd19fc00039e35bfa85c3a0569813d6ce5f69c5
Raphael Cássio de Souza <rapha.cassio@gmail.com>
2020-07-26 16:34:26 -0300

    Added autofocus attribute to input

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { FiGithub } from 'react-icons/fi';
 
 const Input = ({ error, ...rest }) => {
   return (
     <div>
-      <input {...rest} />
+      <FiGithub />
+      <input {...rest} autoFocus />
       {error && <span>{error}</span>}
     </div>
   )
