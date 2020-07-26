60127d944b13b65e6d91e46299cf329ce45ef219
Raphael Cássio de Souza <rapha.cassio@gmail.com>
2020-07-26 17:18:23 -0300

    Imput logic with styles

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
-import { FiGithub } from 'react-icons/fi';
+import React, { useCallback, useRef, useState } from 'react';
+import { FiGithub, FiAlertCircle } from 'react-icons/fi';
+
+import { Container, Tooltip } from './styles'
 
 const Input = ({ error, ...rest }) => {
+  const [isFilled, setIsFilled] = useState(false);
+
+  const inputRef = useRef(null);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFilled(!!inputRef.current.value);
+  }, []);
+
   return (
-    <div>
+    <Container isfilled={isFilled} haserror={!!error}>
       <FiGithub />
-      <input {...rest} autoFocus />
-      {error && <span>{error}</span>}
-    </div>
+      <input ref={inputRef} onBlur={handleInputBlur} {...rest} autoFocus />
+      {error && (
+        <Tooltip>
+          <FiAlertCircle size={20} color="#c53030" />
+          <span>{error}</span>
+        </Tooltip>
+      )}
+    </Container>
   )
 }
 
