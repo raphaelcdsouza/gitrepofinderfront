4137fb19960a4ce48e625a960efbe872e4394e00
Raphael Cássio de Souza <rapha.cassio@gmail.com>
2020-07-28 16:36:09 -0300

    Github callback page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Dashboard from './pages/Dashboard';
+import Callback from './pages/Callback';
 
 import GlobalStyles from './styles/global';
 
@@ -11,6 +12,7 @@ function App() {
       <GlobalStyles />
       <Switch>
         <Route path="/" exact component={Dashboard} />
+        <Route path="/callback" exact component={Callback} />
       </Switch>
     </Router>
   )
---
+++ src/pages/Callback/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Callback = () => {
+  return <h1>Callback page</h1>
+};
+
+export default Callback;
