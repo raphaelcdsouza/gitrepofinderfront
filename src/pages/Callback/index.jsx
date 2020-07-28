53fe96524da4a918986cba31338c487f0aceaae2
Raphael Cássio de Souza <rapha.cassio@gmail.com>
2020-07-28 18:48:18 -0300

    Calling api to authenticate github user

--- src/pages/Callback/index.jsx
+++ src/pages/Callback/index.jsx
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+
+import api from '../../services/api';
+
+import useQuery from '../../hooks/query';
 
 const Callback = () => {
+  const query = useQuery();
+
+  useEffect(() => {
+    async function auth() {
+      const code = query.get('code');
+
+      try { 
+        const response = await api.post('http://localhost:3333/github-authenticate', {
+          code,
+        });
+
+        console.log(response.data);
+      }catch (err) {
+        console.log(err);
+      }
+    }
+
+    auth();
+  }, [query]);
+
   return <h1>Callback page</h1>
 };
 
