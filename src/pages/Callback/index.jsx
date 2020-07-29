896c14aa3ebacc1a002f91a04d97a698173fcfc5
Raphael Cássio de Souza <rapha.cassio@gmail.com>
2020-07-29 20:05:47 -0300

    Callback page styles

--- src/pages/Callback/index.jsx
+++ src/pages/Callback/index.jsx
@@ -1,9 +1,11 @@
 import React, {useEffect} from 'react';
+import { AiOutlineLoading } from 'react-icons/ai';
 import { useHistory } from 'react-router-dom';
-
 
 import useQuery from '../../hooks/query';
 import { useGithubAuth } from '../../hooks/githubAuth';
+
+import { Container } from './styles';
 
 const Callback = () => {
   const query = useQuery();
@@ -24,7 +26,12 @@ const Callback = () => {
     }
   }, [query, setUserData, history, user]);
 
-  return <h1>Callback page</h1>
+  return (
+    <Container>
+      <h1>Configurando conta do Github</h1>
+      <AiOutlineLoading size={40} />
+    </Container>
+  );
 };
 
 export default Callback;
